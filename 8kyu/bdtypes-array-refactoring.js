9412909314e391b36c017ba83f72964f306cb071
denlacky <denlacky@gmail.com>
2023-11-23 00:38:22 +0300

    bdtypes-array-refactoring.js

--- 8kyu/bdtypes-array-refactoring.js
+++ 8kyu/bdtypes-array-refactoring.js
@@ -2,27 +2,35 @@
 //I've written five functions. Each function receives a parameter arr which is an array.
 //Complete the functions using arr inside the function bodies.
 
-//SOLUTION
-function getLength(arr){
+//SOLUTION:
+const arr=['1','2','3']
+
+function getLength(){
     //return length of arr
     return arr.length
 }
-function getFirst(arr){
+function getFirst(){
     //return the first element of arr
     return arr[0]
 }
-function getLast(arr){
+function getLast(){
     //return the last element of arr
     return arr[arr.length-1]
 }
-function pushElement(arr){
+function pushElement(){
     var el=1;
     arr.push(el);
     //push el to arr
     return arr;
 }
-function popElement(arr){
+function popElement(){
     //pop an element from arr
     arr.pop();
     return arr
 }
+
+console.log("getLength:", getLength())
+console.log("getFirst:", getFirst())
+console.log("getLast:", getLast())
+console.log("pushElement:", pushElement())
+console.log("popElement:", popElement())
